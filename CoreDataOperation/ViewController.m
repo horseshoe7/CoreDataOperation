e1aebbf6a9592e6c8d6f8e9c886baeb43ba83bf8
horseshoe7 <ocon.sc@gmail.com>
2015-06-24 11:16:32 +0200

    moved to fetched results controller for updating UI.

--- CoreDataOperation/ViewController.m
+++ CoreDataOperation/ViewController.m
@@ -11,7 +11,13 @@
 #import "CDOUser.h"
 #import "CDORenameUserOperation.h"
 
-@interface ViewController ()
+@interface ViewController ()<NSFetchedResultsControllerDelegate>
+{
+    NSFetchedResultsController *_resultsController;
+}
+@property (nonatomic, weak) IBOutlet UITextField *textField;
+
+@property (nonatomic, readonly) NSFetchedResultsController *resultsController;
 
 @end
 
@@ -19,7 +25,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self createUserIfRequired];  // now we don't really need this other than to create the user.
+    
+    
+    [self.resultsController performFetch:nil];
+    
+    CDOUser *user = [self.resultsController.fetchedObjects firstObject];
+    
+    self.textField.placeholder = user.username;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,10 +41,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)viewDidAppear:(BOOL)animated
+- (void)createUserIfRequired
 {
-    [super viewDidAppear:animated];
-    
     NSManagedObjectContext *context = [NSManagedObjectContext MR_defaultContext];  // I get in the habit of always giving the context, as it forces me to think in threads.  Some devs tell you to hide these details.
     
     CDOUser *onlyUser = [CDOUser MR_findFirstInContext:context];
@@ -38,33 +50,87 @@
         onlyUser = [CDOUser MR_createEntityInContext:context];
         onlyUser.identifier = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
         onlyUser.username = @"Stephen";
-        
         [onlyUser.managedObjectContext MR_saveToPersistentStoreAndWait];
     }
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
     
-    
-    // now we do something with him
-    CDORenameUserOperation *rename = [[CDORenameUserOperation alloc] initWithUser:onlyUser updatedName:@"Stefan Körner"];
-    
-    [rename setCompletionBlock:^{
+}
+
+- (IBAction)renameUser:(id)sender
+{
+    if (self.textField.text.length > 0) {
         
-        // according to the API docs, there is no guarantee this will be called on the main thread.  So, let's guarantee that.
+        // now we do something with him
+        CDORenameUserOperation *rename = [[CDORenameUserOperation alloc] initWithUser:(CDOUser*)[self.resultsController.fetchedObjects firstObject]
+                                                                          updatedName:self.textField.text];
         
-        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-            NSLog(@"Completed!");
-        }];
-        
-        // equivalently, you could use gcd:
-//        dispatch_async(dispatch_get_main_queue(), ^{
+//        __weak ViewController *weakself = self;
+//        [rename setCompletionBlock:^{
 //            
-//        });
+//            // according to the API docs, there is no guarantee this will be called on the main thread.  So, let's guarantee that.
+//            
+//            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//                NSLog(@"Completed!");
+//                
+//                weakself.textField.placeholder = [weakself user].username;
+//                weakself.textField.text = nil;
+//                
+//            }];
+//            
+//            // equivalently, you could use gcd:
+//            //        dispatch_async(dispatch_get_main_queue(), ^{
+//            //
+//            //        });
+//            
+//        }];
         
-    }];
+        NSLog(@"Will add to queue now");
+        
+        [[CDOApplication application].backgroundQueue addOperation:rename];
+        
+    }
+}
+
+#pragma mark - NSFetchedResultsController Related
+
+- (NSFetchedResultsController*)resultsController
+{
+    if (!_resultsController) {
+        
+        NSManagedObjectContext *context = [NSManagedObjectContext MR_defaultContext]; // always on the main thread!
+        
+        NSFetchRequest *request = [CDOUser MR_requestAllSortedBy:@"username" ascending:YES inContext:context];
+        
+        NSFetchedResultsController *controller = nil;
+        controller = [[NSFetchedResultsController alloc] initWithFetchRequest:request
+                                                         managedObjectContext:context
+                                                           sectionNameKeyPath:nil
+                                                                    cacheName:nil];
+        
+        controller.delegate = self;  // DON'T FORGET!!
+        _resultsController = controller;
+        
+    }
+    return _resultsController;
+}
+
+- (void)controller:(NSFetchedResultsController *)controller
+   didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath
+     forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath
+{
     
-    NSLog(@"Will add to queue now");
-    
-    [[CDOApplication application].backgroundQueue addOperation:rename];
-    
+    if ([anObject isKindOfClass:[CDOUser class]]) {
+        CDOUser *user = (CDOUser*)anObject;
+        
+        self.textField.placeholder = user.username;
+        self.textField.text = nil;
+    }
 }
 
 @end
